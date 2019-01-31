70845a0d34dc728a2c65bd0d8a7f8955168a918b
Sara Walker <sarawall1187@gmail.com>
2019-01-31 21:38:03 +0000

    save #list_methods

--- lib/JewelryStore/cli.rb
+++ lib/JewelryStore/cli.rb
@@ -22,21 +22,17 @@ class JewelryStore::CLI
        url = "https://www.jewelry.com/rings"
        JewelryStore::Scraper.scrape_jewelry(url, "Ring")
         list_rings
-        #seperate method 
-        # input = gets.strip 
-        #take input to return that pieces info 
-        #look at @@all_rings and use the number to return the right ring 
-        #pass that ring to the scraper 
+       
       puts "I will have your info about rings shortly."
      when 'earrings' || 'earring'
        url = "https://www.jewelry.com/earrings"
        JewelryStore::Scraper.scrape_jewelry(url, "Earrings")
-       list_jewelry
+       list_earrings
       puts "I will have your info about earrings shortly."
      when 'necklace' || 'necklaces'
        url = "https://www.jewelry.com/necklaces"
        JewelryStore::Scraper.scrape_jewelry(url, "Necklaces")
-       list_jewelry
+       list_necklaces
       puts "I will have your info about necklaces shortly."
      else 
        puts "I did not understand your input."
@@ -65,4 +61,21 @@ class JewelryStore::CLI
     end
    end
    
+    def list_earrings
+     JewelryStore::Jewelry_Piece.all_earrings.each.with_index(1) do |type, i|
+     puts "#{i}. #{type.description}"
+    end
+   end
+   
+    def list_necklaces
+     JewelryStore::Jewelry_Piece.all_necklaces.each.with_index(1) do |type, i|
+     puts "#{i}. #{type.description}"
+    end
+   end
+  # def jewelry_choice
+  #     input = gets.strip 
+  #       #take input to return that pieces info 
+  #       #look at @@all_rings and use the number to return the right ring 
+  #       #pass that ring to the scraper 
+  # end
 end
